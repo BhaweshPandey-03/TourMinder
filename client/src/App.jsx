41834f30909280b78b1c003c7d782caf0bb87244
Akash Yadav <150064374+Akash4317@users.noreply.github.com>
2024-04-18 12:24:45 +0530

    added folder and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 // import React from 'react'
 
+import Home from "./pages/Home"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Home />
+    </div>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/home.css"
+
+const Home = () => {
+    return (
+        <div>
+            <h1>this is home</h1>
+        </div>
+    )
+}
+
+export default Home
