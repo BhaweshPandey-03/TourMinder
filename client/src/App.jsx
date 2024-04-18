67769c3b9558102ec3e7c9fcb51110733f89ee98
Raushan8505 <raushanrajsingh46@gmail.com>
2024-04-18 15:41:26 +0530

    Login Signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 
+import AllRoutes from "./AllRoute/AllRoutes"
 import Home from "./pages/Home"
 
 const App = () => {
   return (
     <div>
       <Home />
+      <AllRoutes/>
     </div>
   )
 }
