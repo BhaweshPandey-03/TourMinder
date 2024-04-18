eb2271a04f64614a4d81309c8d17bebf7a851a94
BhaweshPandey-03 <bhaweshpandey0310@gmail.com>
2024-04-18 16:04:54 +0530

    data fetched

--- server/routes/ai.route.js
+++ server/routes/ai.route.js
@@ -6,10 +6,25 @@ require("dotenv").config();
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
+const formate = {
+  days : [
+    {
+      day01: "<data for day01>"
+    },
+    {
+      day02: "<data for day01>"
+    },
+    {
+      "nth day": "<data for nth day >"
+    },
+  ],
+
+}
+
 aiRouter.post("/tour-planner", async (req, res) => {
   try {
     const { from, startDate, endDate, to } = req.body;
-    const prompt = `plan a trip from ${from} to ${to} from date ${startDate} to ${endDate} `;
+    const prompt = `plan a trip from ${from} to ${to} from date ${startDate} to ${endDate} And i want the response in this formate only ${formate} `;
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
