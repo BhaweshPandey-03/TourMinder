d5e145e6b49712329c7cdc5fe74b59d37dc60902
BhaweshPandey-03 <bhaweshpandey0310@gmail.com>
2024-04-19 00:31:51 +0530

    aa

--- client/src/component/Form.jsx
+++ client/src/component/Form.jsx
@@ -1,6 +1,11 @@
 import { Button, colors } from '@mui/material';
 import React, { useState } from 'react';
 import '../styles/form.css';
+// import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+// import Button from '@mui/material/Button';
+import SimpleBackdrop from './SbtButton';
 
 const Form = () => {
   const [from, setFrom] = useState('');
@@ -8,7 +13,13 @@ const Form = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [tours, setTours] = useState('');
-
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
   const fetchData = async (body) => {
     try {
       const res = await fetch('http://localhost:4500/tour-planner', {
@@ -20,6 +31,7 @@ const Form = () => {
       console.log(typeof data);
       console.log(data);
       setTours(data.text);
+      handleClose();
     } catch (error) {
       console.log(error);
     }
@@ -45,7 +57,7 @@ const Form = () => {
       <h3>
         <span>Make Plan Now !</span>
       </h3>   
-      <form className='form' onSubmit={handleSubmit}>
+      <form className='b-form' onSubmit={handleSubmit}>
         <label htmlFor="from">Your Place</label>
         <input type="text" id="from" value={from} placeholder="Enter your place..." onChange={(e) => setFrom(e.target.value)} />
         <label htmlFor="to">Your Destination</label>
@@ -56,7 +68,21 @@ const Form = () => {
         <label htmlFor="endDate">End Date</label>
         <input type="date" id="endDate" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
 
-        <Button type="submit">Submit</Button>
+        {/* <Button type="submit">Submit</Button> */}
+        {/* <SimpleBackdrop  /> */}
+        {
+          <div>
+      <Button type='submit' onClick={handleOpen}>submit</Button>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
+        }
+        
       </form>
   
       <div className='data'>
